64beddea5a2c78f729f1d29e03bb4893a9384add
Erick Lincoln <erick.lincoln@dataside.com.br>
2025-07-25 11:27:41 -0300

    primeiro commit2345678901

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ from taverna import app
 from taverna.routes import *
 
 # os cadastros utilizarão "SQL ALCHEMY DO FLASK"
-# os formulários utilizarão "WTF FORMS FLASK  "
+# os formulários utilizarão "WTF FORMS FLASK"
+
+import os
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
